a767fc9271740ae3d582ca82480a586e61ef1bbf
anmol dhanda <rk352714@gmail.com>
2024-04-19 21:43:01 +0530

    final commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const toast_notification_btn = document.querySelector(".toast-notification-btn");
+const toastnotification_container = document.getElementById("toastnotification-container");
+const notification_messages = ["👤 user started following you","🖤 user liked your post","🤳 user mentioned you in their post","🙅‍♂️ user unfollowed you","💰 your post got viral","💬 user messaged you"];
+
+function show_notifications(message = null) {
+    let toastnotification_element = document.createElement("div");
+    toastnotification_element.classList.add("toastnotification");
+    if(toastnotification_element.innerText = "") {
+        toastnotification_element.innerText = message;
+    }
+    else {
+        toastnotification_element.innerText = print_random_message();
+    }
+    toastnotification_container.appendChild(toastnotification_element);
+    setTimeout(() => {
+        toastnotification_element.remove();
+    }, 3000);
+}
+toast_notification_btn.addEventListener("click",show_notifications);
+
+// =================================================== iterating the array items & setting the random item as text for our toast notification text ===================================================
+function print_random_message() {
+    return notification_messages[Math.floor(Math.random() * notification_messages.length)];
+}
